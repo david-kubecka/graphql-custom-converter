f2176bc777bbc53c81e1a4c0f5b6a919c0d1f1cf
David Kubecka <davidkubecka366@gmail.com>
2022-11-20 13:02:45 +0100

    init graphql project

---
+++ src/main/kotlin/com/example/graphqlcustomconverter/GraphqlCustomConverterApplication.kt
@@ -0,0 +1,11 @@
+package com.example.graphqlcustomconverter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlCustomConverterApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlCustomConverterApplication>(*args)
+}
